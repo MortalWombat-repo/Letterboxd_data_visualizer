f2946e2a801d955a0311b0803a5805b818bbed85
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2024-04-18 00:33:56 +0200

    Delete mage directory

--- mage/partition_bigquery_warehouse.sql
+++
@@ -1,8 +0,0 @@
---Docs: https://docs.mage.ai/guides/sql-blocks
---Write SQL to create a partitioned table
-CREATE OR REPLACE TABLE `core-forklift-412322.letterboxd.movies_partitioned`
-PARTITION BY DATE(year)
-AS
-SELECT *,
-       date AS year
-FROM {{ df_1 }};
